96d96ef1707a5bb9afea301d0a3bb377f713fe93
ShivanshKaintura <kainturashi@gmail.com>
2025-04-26 02:18:55 +0530

    hello

--- dev/src/main/java/com/sk/dev/Controller/HelloWorldController.java
+++ dev/src/main/java/com/sk/dev/Controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.sk.dev.Controller;
 
+import com.sk.dev.Service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloWorldController {
 
+    @Autowired
+    private EmailService emailService;
+
     @GetMapping("/hello")
     public String hello(Model model) {
+        emailService.sendSimpleEmail(
+                "kainturashi@gmail.com",
+                "API Triggered",
+                "Hey! Your API endpoint was just hit."
+        );
         model.addAttribute("message", "Hello World");
         return "hello"; // This will look for a template called 'hello.html' under resources/templates.
     }
