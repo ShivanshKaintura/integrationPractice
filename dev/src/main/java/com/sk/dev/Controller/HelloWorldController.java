4f1311258b59fd959a35ff76d758c2b6c6b04bd3
ShivanshKaintura <kainturashi@gmail.com>
2025-05-30 00:20:58 +0530

    email removed

--- dev/src/main/java/com/sk/dev/Controller/HelloWorldController.java
+++ dev/src/main/java/com/sk/dev/Controller/HelloWorldController.java
@@ -15,24 +15,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello(Model model, HttpServletRequest request) {
-
-        // Extract the email address from the 'X-User-Email' header
-        String fromEmail = "";
-
-        String clientIp = request.getRemoteAddr();
-
-        if ("0:0:0:0:0:0:0:1".equals(clientIp)) {
-            clientIp = "127.0.0.1"; // Replace with a standard IPv4 loopback address
-        }
-        fromEmail = "user-" + clientIp.replace(".", "-") + "@gmail.com";
-        if (fromEmail == null || fromEmail.isEmpty()) {
-            fromEmail = "kainturashi@gmail.com";
-        }
-        emailService.sendSimpleEmail(
-                fromEmail,
-                "API Triggered",
-                "Hey! Your API endpoint was just hit."
-        );
         model.addAttribute("message", "Hello World");
         return "hello"; // This will look for a template called 'hello.html' under resources/templates.
     }
